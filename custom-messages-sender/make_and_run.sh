e3119c01855e15a965395f11554e131a454ca18a
Antonio Gelain <antonio.gelain2@gmail.com>
2024-07-11 22:32:10 +0200

    chore: update make and run script

--- custom-messages-sender/make_and_run.sh
+++ custom-messages-sender/make_and_run.sh
@@ -1,5 +1,11 @@
-rm -rf build
-cmake -S . -B build
-cd build
-make -j4
+#!/bin/bash
+
+SRC_DIR='.'
+BUILD_DIR='build'
+
+# Build
+cmake -S "$SRC_DIR" -B "$BUILD_DIR"
+cd build && make -j4
+
+# Run custom message sender
 ./custom_message_sender
